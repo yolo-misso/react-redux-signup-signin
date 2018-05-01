a5ad8b701700328aba265f3f9cb6f2a1507379f3
yolo-misso <yolomisso@gmail.com>
2018-05-01 14:39:22 +0530

    sign up, sign in , sign out using local state and firebase API

--- react-redux-signup-signin/src/components/App.js
+++ react-redux-signup-signin/src/components/App.js
@@ -11,56 +11,63 @@ import AccountPage from './Account';
 import * as routes from '../constants/routes';
 
 import './App.css';
+import { firebase } from '../firebase';
 
-/*class App extends Component {
+class App extends Component {
+  constructor (props){
+    super(props);
+
+    this.state = {
+      authUser: null,
+    };
+  }
+
+  componentDidMount(){
+    firebase.auth.onAuthStateChanged(authUser=>{
+      authUser
+        ?this.setState(()=>({authUser}))
+        :this.setState(()=>({authUser:null}));
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}*/
+      <Router>
+        <div>
+        <Navigation authUser={this.state.authUser}/>
 
-const App = ()=>
-
-<Router>
-  <div>
-  <Navigation/>
-
-  <hr/>
-  {/*matches the route and displays the component on the same page*/}
-  <Route
+        <hr/>
+        {/*matches the route and displays the component on the same page*/}
+      <Route
     exact path={routes.LANDING}
     component={()=> <LandingPage/>}
-  />
-  <Route
+      />
+      <Route
     exact path={routes.SIGN_UP}
     component={()=> <SignUpPage/>}
-  />
-  <Route
+      />
+      <Route
     exact path={routes.SIGN_IN}
     component={()=> <SignInPage/>}
-  />
-  <Route
+      />
+      <Route
     exact path={routes.PASSWORD_FORGET}
     component={()=> <PasswordForgetPage/>}
-  />
-  <Route
+      />
+      <Route
     exact path={routes.HOME}
     component={()=> <HomePage/>}
-  />
-  <Route
+      />
+      <Route
     exact path={routes.ACCOUNT}
     component={()=> <AccountPage/>}
-  />
-  </div>
-</Router>
+      />
+    </div>
+  </Router>
+
+    );
+  }
+}
+
 
 export default App;
--- react-redux-signup-signin/src/components/Navigation.js
+++ react-redux-signup-signin/src/components/Navigation.js
@@ -2,15 +2,28 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import * as routes from '../constants/routes';
+import SignOutButton from './SignOut';
 
-const Navigation = ()=>
+const Navigation = ({ authUser }) =>
   <div>
-    <ul>
-      <li><Link to ={routes.SIGN_IN}>Sign In</Link></li>
-      <li><Link to ={routes.LANDING}>Landing</Link></li>
-      <li><Link to ={routes.HOME}>Home</Link></li>
-      <li><Link to ={routes.ACCOUNT}>Account</Link></li>
-    </ul>
-  </div> 
+    { authUser
+        ? <NavigationAuth />
+        : <NavigationNonAuth />
+    }
+  </div>
 
-  export default Navigation;
+const NavigationAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.HOME}>Home</Link></li>
+    <li><Link to={routes.ACCOUNT}>Account</Link></li>
+    <li><SignOutButton /></li>
+  </ul>
+
+const NavigationNonAuth = () =>
+  <ul>
+    <li><Link to={routes.LANDING}>Landing</Link></li>
+    <li><Link to={routes.SIGN_IN}>Sign In</Link></li>
+  </ul>
+
+export default Navigation;
